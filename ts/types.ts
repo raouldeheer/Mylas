763b1c2248b02440c2d02c7f29f1a1899f11ca5d
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-02-15 20:33:01 +0100

    Changed enum Method to const enum Method

--- ts/types.ts
+++ ts/types.ts
@@ -1,13 +1,13 @@
-/** @internal */
+
 export declare type voidCallback = () => void;
-/** @internal */
+
 export declare type stringCallback = (arg0: string) => void;
-/** @internal */
+
 export declare type objectCallback<T> = (arg0: T) => void;
-/** @internal */
+
 export declare type booleanCallback = (arg0: boolean) => void;
-/** @internal */
-export enum Method {
+
+export const enum Method {
     loadFile,
     saveFile,
     loadJson,
@@ -16,7 +16,7 @@ export enum Method {
     loadBuffer,
     saveBuffer,
 }
-/** @internal */
+
 export interface Request<T = unknown> {
     method: Method,
     path: string,
