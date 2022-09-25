83c2ba94f3dd9bc5df994e994f3a8fad0d7cdfa8
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-09-25 22:04:12 +0200

    ➡Moved dts to directory

--- config/build.sh
+++ config/build.sh
@@ -3,6 +3,4 @@
 rm -rf ./build
 tsc --build ./config/tsconfig.json
 tsup --config config/tsup.config.ts
-cp ./ts/index.d.ts ./build/index.d.ts
-cp ./ts/register.d.ts ./build/register.d.ts
-cp ./ts/interfaces.d.ts ./build/interfaces.d.ts
+cp ./ts/dts/* ./build
