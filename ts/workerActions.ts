c1a4469e8f20a9ee81e46ced851b3c769963e737
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-09-25 20:56:06 +0200

    🩹Fixed worker location

--- ts/workerActions.ts
+++ ts/workerActions.ts
@@ -1,12 +1,13 @@
-import { Worker } from 'worker_threads';
+import { Worker } from "worker_threads";
 import { Request } from "./types";
+import { resolve } from "path";
 const action = <T>(
     { callback, ...req }: Request<T>
 ): Promise<T> => new Promise<T>((res, rej) => {
     let d: T;
-    new Worker('./build/worker.js')
-        .once('message', m => d = m).once('exit', c => c == 0 ? res(d) : rej(c))
-        .once('error', e => rej(e)).once('messageerror', e => rej(e))
+    new Worker(resolve(__dirname, "./worker.js"))
+        .once("message", m => d = m).once("exit", c => c == 0 ? res(d) : rej(c))
+        .once("error", e => rej(e)).once("messageerror", e => rej(e))
         .postMessage(req);
 }).then(v => { callback?.(v); return v; }, e => { throw new Error(e); });
 export default action;
